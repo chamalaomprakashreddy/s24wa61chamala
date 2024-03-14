9a99d3f0624c19d13ce37606a237b9d61110b949
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-03-14 12:59:08 -0500

    updated

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Omprakashreddy chamala' });
+  res.render('mydata', { title: 'Omprakashreddy chamala' });
 });
 
 module.exports = router;
