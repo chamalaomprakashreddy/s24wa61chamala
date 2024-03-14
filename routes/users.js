96f0eebfa6c63fa4f0be3ddc4f89a797154f2e66
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-03-14 13:04:49 -0500

    updated

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let product = 1;
+let factor = 1;
+router.get('/', function(req, res, next) {
+  factor++;
+  product *= factor;
+  res.send(`Product is: ${product}`);
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
