ada697ae669a30ee97f8c3fac61e86f89a9e0840
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-03-18 20:01:16 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,7 @@ const a = Math.random();
 const b = Math.random();
 router.get('/', function(req, res) {
     const difference = Math.abs(a - b);
-    res.send(`Absolute difference between ${a} and ${b} is ${difference}`);
+    res.send(`Math.abs(a - b) applied to ${a} and ${b} is ${difference}`);
 });
 router.get('/', function(req, res) {
     res.send('Respond with a resource');
