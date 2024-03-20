790b5e7f71a61c2f640451c8ed30551653acbe2d
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-03-20 13:37:47 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// let x = req.query.x ? parseFloat(req.query.x) : Math.random();
-// let y = Math.random();
-// router.get('/', function(req, res) {
-//     const difference = Math.abs(x - y);
-//     res.send(`Math.abs(x - y) applied to ${x} and ${y} is ${difference}`);
-// });
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 
